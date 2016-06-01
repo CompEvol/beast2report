ed3ed7f58af6845a0bde54a9de28f90c7a906813
Tim Vaughan <tgvaughan@gmail.com>
2016-06-02 11:26:21 +1200

    genIssueDist.sh now allows time range limiting.

--- genIssueDist.sh
+++ genIssueDist.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
+#
+# Run this script from the beast2 git repository.  It will generate
+# two files:
+#
+#   issue_files: a sorted list of files affected by issue related commits, and
+#   issue_dist: a table containing the number of issues related to each file
+#
+# To limit the time period used in generating these files, use the --since DATE
+# command line parameter.  For example, to consider only those commits in the
+# last year (assuming the current date is June 2016) use
+#
+#   ./genIssueDist.sh --since 06/2015
+#
 
 echo -n "Extracting names of files affected by issue-related commits..."
-for issue in `git log --oneline | grep -E -o '#[0-9]+' | sort | uniq`; do
+for issue in `git log --oneline "$@" | grep -E -o '#[0-9]+' | sort | uniq`; do
     issueNum=`echo $issue | cut -d# -f2`
-    for commit in `git log --oneline | grep -E $issue'([^0-9]|$)' | cut -d' ' -f1`; do
+    for commit in `git log --oneline "$@" | grep -E $issue'([^0-9]|$)' | cut -d' ' -f1`; do
         for file in `git show --oneline $commit --name-only | tail -n+2`; do
             echo $issueNum $file
         done
